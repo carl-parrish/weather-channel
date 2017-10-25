e14ae968f2ff30609b9b4fc1f96d1a812a868669
cparrish <carl.parrish@gmail.com>
2017-10-25 14:06:05 -0700

    Test spinner

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
-import {TemperaturePipe } from './temperature.pipe';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
