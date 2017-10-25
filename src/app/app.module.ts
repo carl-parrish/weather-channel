bd95bdfe7040513def66870c8a03dfdbca765707
cparrish <carl.parrish@gmail.com>
2017-10-25 10:43:34 -0700

    Add spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NgModule } from '@angular/core';
 import { WeatherService } from './services/weather.service';
+import { Angular2FontawesomeModule } from 'angular2-fontawesome';
 
 import { AppComponent } from './app.component';
 import { TemperaturePipe } from './temperature.pipe';
@@ -15,7 +16,8 @@ import { TemperaturePipe } from './temperature.pipe';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    Angular2FontawesomeModule
   ],
   providers: [ WeatherService ],
   bootstrap: [AppComponent]
