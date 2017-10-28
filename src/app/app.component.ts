c31e63ac381050e5d49a78c1a5e9e7e8e604c904
cparrish <carl.parrish@gmail.com>
2017-10-27 19:18:13 -0700

    Fix for background image bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,8 +32,8 @@ export class AppComponent implements OnInit {
             this.icon = weather.icon;
             // this.temperature = main.temp;
             this.temperature = 40;
-            this.tempInCelsius = new TemperaturePipe().transform(main.temp, 'C');
-            this.tempInFahrenheit = new TemperaturePipe().transform(this.celsius2Fahrenheit(main.temp), 'F');
+            this.tempInCelsius = new TemperaturePipe().transform(this.temperature, 'C');
+            this.tempInFahrenheit = new TemperaturePipe().transform(this.celsius2Fahrenheit(this.temperature), 'F');
           });
       });
     }
