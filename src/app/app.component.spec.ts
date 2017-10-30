11b7916a0b6d62c69b133907c3f7975aacc173ce
cparrish <carl.parrish@gmail.com>
2017-10-30 11:43:50 -0700

    Fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,21 @@
+import { Angular2FontawesomeModule } from 'angular2-fontawesome';
+import { HttpModule } from '@angular/http';
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { WeatherService } from './services/weather.service';
+
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ Angular2FontawesomeModule, HttpModule ],
       declarations: [
         AppComponent
       ],
+      providers: [
+        WeatherService
+      ]
     }).compileComponents();
   }));
 
@@ -21,13 +29,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Carl\'s Angular Weather Project');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
   }));
 
   it('should toggle showCelsius value', async(() => {
